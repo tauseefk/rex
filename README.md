8c4255bc1b6cdaca7de70fb7ecb9b7fe2ffa28e0
Tauseef Khan <tauseef25@gmail.com>
2025-02-03 21:57:17 -0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Rex
 Tiny arms but bytes.
+
+```
+// Explanation:
+// - `Stream.fromInterval(100)` creates a stream that emits a value every 100 milliseconds.
+// - `skipN(10)` creates a filter that allows only every 10th value to pass through.
+const $oneSecStream = Stream.fromInterval(100).filter(skipN(10));
+
+// - The stream will print emitted values until the emitted value is 50, then unsubscribes.
+// - Once unsubscribed or naturally completed, the completion handler is executed.
+$oneSecStream.subscribe({
+  next: (tick) => {
+    console.log(tick);
+    if (tick >= 50) $oneSecStream.unsubscribe?.();
+  },
+  complete: () => {
+    console.log('stream concluded');
+  },
+});
+
+```
--- examples.ts
+++ examples.ts
@@ -11,12 +11,17 @@ const skipN = (n: number) => {
   };
 };
 
+// Explanation:
+// - `Stream.fromInterval(100)` creates a stream that emits a value every 100 milliseconds.
+// - `skipN(10)` creates a filter that allows only every 10th value to pass through.
 const $oneSecStream = Stream.fromInterval(100).filter(skipN(10));
 
+// - The stream will print emitted values until the emitted value is 50, then unsubscribes.
+// - Once unsubscribed or naturally completed, the completion handler is executed.
 $oneSecStream.subscribe({
   next: (tick) => {
     console.log(tick);
-    if (tick >= 5) $oneSecStream.unsubscribe?.();
+    if (tick >= 50) $oneSecStream.unsubscribe?.();
   },
   complete: () => {
     console.log('stream concluded');
